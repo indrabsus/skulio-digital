76bacd094d72a61c611912c32e88351c63d3f472
bataravix3r <bataravix3r@gmail.com>
2023-10-29 21:54:24 +0700

    fix bug migrate

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,23 @@ use App\Livewire\Admin\Dashboard;
 use App\Models\Menu;
 use Illuminate\Support\Facades\Route;
 
-
 //Login Page
 Route::get('/',[AuthController::class,'loginpage'])->name('loginpage');
 Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+
 //Proses Login
 Route::post('loginauth',[AuthController::class,'login'])->name('loginauth');
 
 Route::group(['middleware' => ['auth']], function(){
-    Route::get('admin.dashboard',Dashboard::class)->name('admin.dashboard');
+    Route::get('admin.dashboard', Dashboard::class)->name('admin.dashboard');
+
+    // Now, move the Menu::all() and route definition here
+    $data = Menu::all();
+    foreach ($data as $item) {
+        // Determine the middleware based on $item->parent
+        $middleware = 'cekrole:' . $item->akses_role;
+
+        // Define the route without grouping
+        Route::middleware($middleware)->get($item->path, $item->class)->name($item->name);
+    };
 });
-
-$data = Menu::all();
-foreach ($data as $item) {
-    // Determine the middleware based on $item->parent
-    $middleware = 'cekrole:' . $item->akses_role;
-
-    // Define the route without grouping
-    Route::middleware($middleware)->get($item->path, $item->class)->name($item->name);
-}
-
