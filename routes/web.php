f7a16fd6f5078b4d2833c5b0d7a73598bbf8cbc0
indrabsus <bataravix3r@gmail.com>
2023-11-06 17:20:59 +0700

    merge fergie

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[AuthController::class,'loginpage'])->name('loginpage');
 Route::get('/logout',[AuthController::class,'logout'])->name('logout');
 
-
 //Proses Login
 Route::post('loginauth',[AuthController::class,'login'])->name('loginauth');
 
