8e53dfd7ab15885cc8bffe7f44554764753516b1
IsekaiRid <mainrpg62@gmail.com>
2023-11-19 08:31:31 +0700

    sistemLaporan

--- app/Livewire/BankMini/LaporanTabungan.php
+++ app/Livewire/BankMini/LaporanTabungan.php
@@ -5,6 +5,7 @@ namespace App\Livewire\BankMini;
 use App\Models\DataSiswa;
 use Livewire\Component;
 use App\Models\LaporanTabungan as TabelLaporan;
+use App\Models\LaporanTabungan as ModelsLaporanTabungan;
 use Illuminate\Support\Facades\DB;
 use Livewire\WithPagination;
 
@@ -17,13 +18,21 @@ class LaporanTabungan extends Component
     public $result = 10;
     public function render()
     {
+        $this->sumkredit = DB::table('log_tabungan')->where('jenis', 'db')->sum('nominal');
+
+        $this->sumdebit = DB::table('log_tabungan')->where('jenis', 'kd')->sum('nominal');
+
         $this->count = DB::table('log_tabungan')->distinct()->pluck('id_siswa')->count();
 
         $data  = TabelLaporan::orderBy('id_laporan','desc')->where('jumlah_nasabah', 'like','%'.$this->cari.'%')->paginate($this->result);
         return view('livewire.bank-mini.laporan-tabungan', compact('data'));
     }
 
+
+    public $sumdebit;
+    public $sumkredit;
     public $count;
+
 
 
 }
--- resources/views/livewire/bank-mini/laporan-tabungan.blade.php
+++ resources/views/livewire/bank-mini/laporan-tabungan.blade.php
@@ -7,18 +7,6 @@
                 <div class="card-body">
                   <div class="d-flex justify-content-between align-items-baseline">
                     <h6 class="card-title mb-0">Jumlah Nasabah</h6>
-                    <div class="dropdown mb-2">
-                      <a type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
-                      </div>
-                    </div>
                   </div>
                   <div class="row">
                     <div class="col-6 col-md-12 col-xl-5">
@@ -42,22 +30,10 @@
                 <div class="card-body">
                   <div class="d-flex justify-content-between align-items-baseline">
                     <h6 class="card-title mb-0">Pemasukan Debit</h6>
-                    <div class="dropdown mb-2">
-                      <a type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
-                      </div>
-                    </div>
                   </div>
                   <div class="row">
                     <div class="col-6 col-md-12 col-xl-5">
-                      <h3 class="mb-2">10</h3>
+                      <h3 class="mb-2">Rp.{{number_format( $sumsebit ,0,',','.')}}</h3>
                       <div class="d-flex align-items-baseline">
                         <p class="text-danger">
                           <span>-2%</span>
@@ -77,22 +53,10 @@
                 <div class="card-body">
                   <div class="d-flex justify-content-between align-items-baseline">
                     <h6 class="card-title mb-0">pengluaran Kredit</h6>
-                    <div class="dropdown mb-2">
-                      <a type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
-                      </div>
-                    </div>
                   </div>
                   <div class="row">
                     <div class="col-6 col-md-12 col-xl-5">
-                      <h3 class="mb-2">8</h3>
+                      <h3 class="mb-2">Rp.{{number_format( $sumkredit ,0,',','.')}}</h3>
                       <div class="d-flex align-items-baseline">
                         <p class="text-success">
                           <span>+2.8%</span>
