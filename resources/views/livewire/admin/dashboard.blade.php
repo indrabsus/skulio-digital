59116a5b710a6a13a091f53061f3f716f3cdcef0
indrabsus <bataravix3r@gmail.com>
2024-01-26 07:52:48 +0700

    hapus dashboard

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -3,13 +3,4 @@
     <hr>
     <p>Aplikasi ini dibuat oleh Tim IT SMK Sangkuriang 1 Cimahi. Dalam aplikasi skulio, rekan-rekan bisa mengatur Manajemen Informasi seputar sekolah</p>
     <p>Silakan gunakan aplikasi Skulio Pro dengan Bijak</p>
-    <br>
-    <h5>Developer:</h5>
-    <ul>
-        <li>Indra Batara, S.Pd</li>
-        <li>Ridhwan X PPLG 1</li>
-        <li>Fergie XI PPLG 3</li>
-        <li>Rizki Ramadhan XI PPLG 4</li>
-        <li>Fathur XI PPLG 1</li>
-    </ul>
 </div>
