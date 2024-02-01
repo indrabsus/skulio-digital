f01ae809b641ed68462263f7535976450670356b
indrabsus <bataravix3r@gmail.com>
2024-02-02 05:51:12 +0700

    nilaiku

---
+++ app/Livewire/Siswa/Nilaiku.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Siswa;
+
+use Livewire\Component;
+
+class Nilaiku extends Component
+{
+    public function render()
+    {
+        return view('livewire.siswa.nilaiku');
+    }
+}
