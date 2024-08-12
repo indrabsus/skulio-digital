4a26ec2e4633bde5f76032fe9a9ac9c30d33f099
indrabsus <bataravix3r@gmail.com>
2024-08-12 17:47:05 +0700

    coba fingerprint

--- app/Http/Controllers/FingerPrint.php
+++ app/Http/Controllers/FingerPrint.php
@@ -8,7 +8,7 @@ use Rats\Zkteco\Lib\ZKTeco;
 class FingerPrint extends Controller
 {
     public function rawlogsc(){
-        $zk = new ZKTeco('33.33.33.33');
+        $zk = new ZKTeco('88.88.88.88');
         dd($zk->connect());
         $log = $zk->getAttendance();
 
