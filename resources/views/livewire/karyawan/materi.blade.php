274ce3bd98ff43184d9880758fe620f7f0a2d679
indrabsus <bataravix3r@gmail.com>
2024-08-29 12:08:40 +0700

    hapus hapus

--- resources/views/livewire/karyawan/materi.blade.php
+++ resources/views/livewire/karyawan/materi.blade.php
@@ -141,7 +141,7 @@
 
                                 @if(Auth::user()->id_role != 5)
                                     <a href="" class="btn btn-success btn-xs" data-bs-toggle="modal" data-bs-target="#edit" wire:click='edit("{{$d->id_materi}}")'><i class="fa-solid fa-edit"></i></i></a>
-                                    <a href="" class="btn btn-danger btn-xs" data-bs-toggle="modal" data-bs-target="#k_hapus" wire:click="c_delete('{{$d->id_materi}}')"><i class="fa-solid fa-trash"></i></a>
+                                    {{-- <a href="" class="btn btn-danger btn-xs" data-bs-toggle="modal" data-bs-target="#k_hapus" wire:click="c_delete('{{$d->id_materi}}')"><i class="fa-solid fa-trash"></i></a> --}}
                                  @else
                                     <button href="" class="btn btn-success btn-xs" data-bs-toggle="modal" data-bs-target="#konf" wire:click='konf("{{$d->id_materi}}")' {{ $d->keterangan ? 'disabled' : '' }}>Konfirmasi</button>
 
