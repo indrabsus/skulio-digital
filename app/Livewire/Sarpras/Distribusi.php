fe70e12861d590fae3100f0036147a105afa5cd6
indrabsus <bataravix3r@gmail.com>
2024-09-10 20:07:34 +0700

    siap tayang

--- app/Livewire/Sarpras/Distribusi.php
+++ app/Livewire/Sarpras/Distribusi.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 use App\Models\Distribusi as TabelDistribusi;
 use App\Models\Role;
 use App\Models\Ruangan;
+use Illuminate\Support\Facades\Auth;
 use Livewire\WithPagination;
 
 class Distribusi extends Component
@@ -21,11 +22,21 @@ class Distribusi extends Component
 
         $ruangan = Ruangan::all();
         $role = Role::all();
-        $data  = TabelDistribusi::leftJoin('roles','roles.id_role','distribusi.id_role')
-        ->leftJoin('bos_realisasi','bos_realisasi.id_realisasi','distribusi.id_realisasi')
-        ->leftJoin('pengajuan','pengajuan.id_pengajuan','bos_realisasi.id_pengajuan')
-        ->select('distribusi.*','roles.nama_role','bos_realisasi.id_realisasi','pengajuan.id_pengajuan','pengajuan.nama_barang','pengajuan.nama_kegiatan','pengajuan.tahun_arkas','pengajuan.satuan','pengajuan.jenis','volume_realisasi')
-        ->orderBy('id_distribusi','desc')->where('nama_barang', 'like','%'.$this->cari.'%')->paginate($this->result);
+        if(Auth::user()->id_role == 1 || Auth::user()->id_role == 16 || Auth::user()->id_role == 17){
+            $data  = TabelDistribusi::leftJoin('roles','roles.id_role','distribusi.id_role')
+            ->leftJoin('bos_realisasi','bos_realisasi.id_realisasi','distribusi.id_realisasi')
+            ->leftJoin('pengajuan','pengajuan.id_pengajuan','bos_realisasi.id_pengajuan')
+            ->select('distribusi.*','roles.nama_role','bos_realisasi.id_realisasi','pengajuan.id_pengajuan','pengajuan.nama_barang','pengajuan.nama_kegiatan','pengajuan.tahun_arkas','pengajuan.satuan','pengajuan.jenis','volume_realisasi')
+            ->orderBy('id_distribusi','desc')->where('nama_barang', 'like','%'.$this->cari.'%')->paginate($this->result);
+        } else {
+            $data  = TabelDistribusi::leftJoin('roles','roles.id_role','distribusi.id_role')
+            ->leftJoin('bos_realisasi','bos_realisasi.id_realisasi','distribusi.id_realisasi')
+            ->leftJoin('pengajuan','pengajuan.id_pengajuan','bos_realisasi.id_pengajuan')
+            ->select('distribusi.*','roles.nama_role','bos_realisasi.id_realisasi','pengajuan.id_pengajuan','pengajuan.nama_barang','pengajuan.nama_kegiatan','pengajuan.tahun_arkas','pengajuan.satuan','pengajuan.jenis','volume_realisasi')
+            ->orderBy('id_distribusi','desc')
+            ->where('pengajuan.id_role', Auth::user()->id_role)
+            ->where('nama_barang', 'like','%'.$this->cari.'%')->paginate($this->result);
+        }
 
         return view('livewire.sarpras.distribusi', compact('data','ruangan','role'));
     }
