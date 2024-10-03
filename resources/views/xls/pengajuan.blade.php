836034f943f0e5b8341e0cac419e3392861dd254
indrabsus <bataravix3r@gmail.com>
2024-10-03 21:01:29 +0700

    persenan2

--- resources/views/xls/pengajuan.blade.php
+++ resources/views/xls/pengajuan.blade.php
@@ -27,7 +27,7 @@
             <td style="border: 1px solid black; background-color: #f7f7f7; word-wrap: break-word;">{{ $d->volume_realisasi }}</td>
             <td style="border: 1px solid black; background-color: #f7f7f7; word-wrap: break-word;">{{ $d->perkiraan_harga_realisasi }}</td>
             <td style="border: 1px solid black; background-color: #f7f7f7; word-wrap: break-word;">{{ $d->bulan_pengajuan_realisasi }}</td>
-            <td style="border: 1px solid black; background-color: #f7f7f7; word-wrap: break-word;">{{ number_format($d->perkiraan_harga_realisasi * $d->volume_realisasi,0,',','.') }}</td>
+            <td style="border: 1px solid black; background-color: #f7f7f7; word-wrap: break-word;">{{ $d->perkiraan_harga_realisasi * $d->volume_realisasi }}</td>
             <td style="border: 1px solid black; word-wrap: break-word;">{{ $d->jenis }}</td>
             <td style="border: 1px solid black; word-wrap: break-word;">{{ $d->tahun_arkas }}</td>
             <td style="border: 1px solid black; word-wrap: break-word;">{{ $d->nama_role }}</td>
