f93c785a252c676f81ffc6eac1f02a7158a227ce
Indra Batara <indrabatara@Laptop-Indra.local>
2025-02-18 18:13:47 +0700

    siswa siswi

---
+++ app/Livewire/Kurikulum/AbsenHarian.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Kurikulum;
+
+use Livewire\Component;
+
+class AbsenHarian extends Component
+{
+    public function render()
+    {
+        return view('livewire.kurikulum.absen-harian');
+    }
+}
